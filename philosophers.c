e3f7e990830a3f8ce08ad3ab15b4aa240f3f221a
Saad El fadil <sel-fadi@student.1337.ma>
2021-05-28 21:13:38 +0100

    Start threads..

--- philosophers.c
+++ philosophers.c
@@ -6,11 +6,58 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 10:58:44 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/05/28 20:37:12 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/05/28 21:09:41 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philosophers.h"
+
+int saad = 0;
+
+pthread_mutex_t mutex;
+
+void    *myfunc(void *argv)
+{
+    int i = 0;
+    while (i < 10000000)
+    {
+        pthread_mutex_lock(&mutex);
+        saad++;
+        i++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+void    *myfunction(void *argv)
+{
+    static int i;
+    i += 1;
+    printf("Hello from thread : %d\n", i);
+    return NULL;
+}
+
+int		start_threads(s_philo *philo)
+{
+	pthread_t t[philo->num_of_philo];
+    int i = 0;
+
+    pthread_mutex_init(&mutex, NULL);
+    while (i < philo->num_of_philo)
+    {
+        pthread_create(&t[i], NULL, &myfunc, NULL);
+        i++;
+    }
+    i = 0;
+    while (i < philo->num_of_philo)
+    {
+        pthread_join(t[i], NULL);
+        i++;
+    }
+    pthread_mutex_destroy(&mutex);
+	printf("saad : %d\n", saad);
+	return (0);
+}
 
 int		init_philo(s_philo *philo, char **argv, int argc)
 {
@@ -27,12 +74,6 @@ int		init_philo(s_philo *philo, char **argv, int argc)
 	else
 		if ((philo->notepme = ft_atoi(argv[5])) == 0)
 			return (exit_error("error: bad arguments\n"));
-	return (0);
-}
-
-int		start_threads(s_philo *philo)
-{
-	printf("Iam ready to start those %d threads\n", philo->num_of_philo);
 	return (0);
 }
 
