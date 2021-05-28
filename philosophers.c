2e29b92f2bbb1076955f233a04776919a4468b4a
Saad El fadil <sel-fadi@student.1337.ma>
2021-05-28 20:37:34 +0100

    Init structure done 100%

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 10:58:44 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/05/28 19:46:56 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/05/28 20:37:12 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 int		init_philo(s_philo *philo, char **argv, int argc)
 {
 	if ((philo->num_of_philo = (int) ft_atoi(argv[1])) == 0)
-		return arguments_error();
+		return (exit_error("error: bad arguments\n"));
 	if ((philo->time_to_die = ft_atoi(argv[2])) == 0)
-		return arguments_error();
+		return (exit_error("error: bad arguments\n"));
 	if ((philo->time_to_eat = ft_atoi(argv[3])) == 0)
-		return arguments_error();
+		return (exit_error("error: bad arguments\n"));
 	if ((philo->time_to_sleep = ft_atoi(argv[4])) == 0)
-		return arguments_error();
+		return (exit_error("error: bad arguments\n"));
 	if (argc == 5)
 		philo->notepme = -1;
 	else
 		if ((philo->notepme = ft_atoi(argv[5])) == 0)
-			return arguments_error();
+			return (exit_error("error: bad arguments\n"));
 	return (0);
 }
 
 int		start_threads(s_philo *philo)
 {
-	printf("Hey philos : %d\n", philo->num_of_philo);
+	printf("Iam ready to start those %d threads\n", philo->num_of_philo);
 	return (0);
 }
 
@@ -44,11 +44,6 @@ int	main(int argc, char *argv[])
 	else
 		init_philo(&philo, argv, argc);
 	if (start_threads(&philo))
-		return (exit_error("error: fatal\n"));
-	// printf("1 : %d\n", philo.num_of_philo);
-	// printf("2 : %ld\n", philo.time_to_die);
-	// printf("3 : %ld\n", philo.time_to_eat);
-	// printf("4 : %ld\n", philo.time_to_sleep);
-	// printf("5 : %ld\n", philo.notepme);
+		return (exit_error("error: in threads\n"));
 	return (0);
 }
